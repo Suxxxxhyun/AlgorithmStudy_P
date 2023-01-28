86aef478472607ca845ae19845c38a15e08e1007
suhyun <qkrtngus116@naver.com>
2023-01-28 15:25:16 +0900

    pg.42891(실패)

---
+++ Programmers/src/Level4/Solution42891Test.java
@@ -0,0 +1,12 @@
+package Level4;
+
+public class Solution42891Test {
+    public static void main(String[] args) {
+
+        int[] food_time = {3,5,1,6,5,3};
+        long k = 20;
+
+        Solution42891 s = new Solution42891();
+        s.solution(food_time, k);
+    }
+}

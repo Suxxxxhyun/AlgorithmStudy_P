8e29cd81d42cdda2ff7d8b3675da0f3ec04eb466
suhyun <qkrtngus116@naver.com>
2023-02-03 17:45:08 +0900

    pg.147355(int,long타입주의,실패)

---
+++ Programmers/src/Level1/Solution147355Test.java
@@ -0,0 +1,9 @@
+package Level1;
+
+public class Solution147355Test {
+    public static void main(String[] args) {
+        Solution147355 s = new Solution147355();
+        long result = s.solution("0","0");
+        System.out.println(result);
+    }
+}

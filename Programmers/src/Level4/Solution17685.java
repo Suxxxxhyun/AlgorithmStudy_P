3dfc3219b8800a71c45898e202b2d99677a5f211
suhyun <qkrtngus116@naver.com>
2023-02-10 12:49:55 +0900

    17685(실패)

--- Programmers/src/Level4/Solution17685.java
+++ Programmers/src/Level4/Solution17685.java
@@ -1,7 +1,55 @@
 package Level4;
 
-public class Solution17685 {
-    public static void main(String[] args){
+import java.util.HashMap;
 
+public class Solution17685 {
+    static Node root = new Node();
+
+    static int solution(String[] words) {
+        int answer = 0;
+
+        for(String word: words){
+            insert(word);
+        }
+
+        return search(root, 0);
+    }
+
+    static void insert(String word){
+        Node curNode = root;
+        for(int i = 0; i < word.length(); i++){
+            Node newRoot = curNode.put(word.charAt(i));
+            curNode = newRoot;
+        }
+        curNode.put('*');
+    }
+
+    static int search(Node root, int cnt){
+        if(root.childCount == 1){
+            return cnt;
+        }
+        int count = 0;
+        for(char key: root.childNodes.keySet()){
+            if(key == '*'){
+                count += cnt;
+            }
+            else{
+                count += search(root.childNodes.get(key), cnt+1);
+            }
+        }
+        return count;
+    }
+
+    static class Node{
+        private HashMap<Character, Node> childNodes = new HashMap<>();
+        private int childCount = 0;
+
+        Node put(char c){
+            childCount++;
+            if(!childNodes.containsKey(c)) {
+                childNodes.put(c, new Node());
+            }
+            return childNodes.get(c);
+        }
     }
 }
---
+++ Programmers/src/Level4/Solution17685Test.java
@@ -0,0 +1,10 @@
+package Level4;
+
+public class Solution17685Test {
+    public static void main(String[] args){
+        String[] words = {"word"};
+
+        Solution17685 s = new Solution17685();
+        System.out.println(s.solution(words));
+    }
+}

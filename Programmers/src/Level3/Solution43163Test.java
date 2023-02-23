7f481d513ca97718eea589e095bc6ba418907a78
suhyun <qkrtngus116@naver.com>
2023-02-24 08:57:44 +0900

    pg.43163(성공)

--- Programmers/src/Level3/Solution43163Test.java
+++ Programmers/src/Level3/Solution43163Test.java
@@ -4,7 +4,7 @@ public class Solution43163Test {
     public static void main(String[] args){
         Solution43163 s = new Solution43163();
         String begin = "hit", target = "cog";
-        String[] words = {"hot", "dot", "lot", "cog", "log", "dog"};
+        String[] words = {"hot", "dot", "dog", "lot", "log", "cog"};
 
         System.out.println(s.solution(begin, target, words));
     }

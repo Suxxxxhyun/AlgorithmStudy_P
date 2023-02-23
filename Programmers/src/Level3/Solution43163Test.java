dc79289a4465f44424474d37786ede40774a426f
suhyun <qkrtngus116@naver.com>
2023-02-23 11:42:31 +0900

    pg.43163(실패)

---
+++ Programmers/src/Level3/Solution43163Test.java
@@ -0,0 +1,11 @@
+package Level3;
+
+public class Solution43163Test {
+    public static void main(String[] args){
+        Solution43163 s = new Solution43163();
+        String begin = "hit", target = "cog";
+        String[] words = {"hot", "dot", "lot", "cog", "log", "dog"};
+
+        System.out.println(s.solution(begin, target, words));
+    }
+}

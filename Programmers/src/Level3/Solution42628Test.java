830195f23920eee7ed05ffebe0bf530abd715089
suhyun <qkrtngus116@naver.com>
2023-03-12 17:44:33 +0900

    pg.42628(실패) -다른 블로그 풀이

---
+++ Programmers/src/Level3/Solution42628Test.java
@@ -0,0 +1,9 @@
+package Level3;
+
+public class Solution42628Test {
+    public static void main(String[] args) {
+        String[] operations = {"I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"};
+        Solution4262802 s = new Solution4262802();
+        System.out.println(s.solution(operations));
+    }
+}

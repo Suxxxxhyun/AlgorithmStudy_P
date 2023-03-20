2d35ea981363dc99c38564c4d478c3e1045d29f6
suhyun <qkrtngus116@naver.com>
2023-03-20 10:39:29 +0900

    pg.81301(다른풀이)

---
+++ Programmers/src/Level1/Solution8130102.java
@@ -0,0 +1,16 @@
+package Level1;
+
+import java.util.*;
+class Solution8130102 {
+    public int solution(String s) {
+        int answer = 0;
+        StringBuilder sb = new StringBuilder("");
+        int len = s.length();
+        String[] digits = {"0","1","2","3","4","5","6","7","8","9"};
+        String[] alphabets = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+        for(int i=0; i<10; i++){
+            s = s.replaceAll(alphabets[i],digits[i]);
+        }
+        return Integer.parseInt(s);
+    }
+}

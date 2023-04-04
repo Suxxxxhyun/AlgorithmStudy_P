07e1d27707320afaf5536a83343fbfb290619cde
suhyun <qkrtngus116@naver.com>
2023-04-04 10:04:27 +0900

    pg.17684(실패)

---
+++ Programmers/src/Level2/Solution17684Test.java
@@ -0,0 +1,8 @@
+package Level2;
+
+public class Solution17684Test {
+    public static void main(String[] args){
+        Solution17684 s = new Solution17684();
+        System.out.println(s.solution("KAKAO"));
+    }
+}

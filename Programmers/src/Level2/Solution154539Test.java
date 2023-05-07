a7b3c4cc48290e6641268a26471eda32fd396439
suhyun <qkrtngus116@naver.com>
2023-05-07 11:22:15 +0900

    154539(실패) stack의 numbers의 index정보를 담는 것. 처음 보는 코드

---
+++ Programmers/src/Level2/Solution154539Test.java
@@ -0,0 +1,9 @@
+package Level2;
+
+public class Solution154539Test {
+    public static void main(String[] args){
+        Solution154539 s = new Solution154539();
+        int[] numbers = {8,9,5,6};
+        System.out.println(s.solution(numbers));
+    }
+}

0a1415c0d279780c6b103c82da64643ea89a22f1
suhyun <qkrtngus116@naver.com>
2023-05-08 09:35:26 +0900

    135808(실패)

---
+++ Programmers/src/Level1/Solution13580802.java
@@ -0,0 +1,21 @@
+package Level1;
+
+import java.util.*;
+
+public class Solution13580802 {
+
+    int answer = 0;
+    public int solution(int k, int m, int[] score) {
+
+        Integer[] s = Arrays.stream(score).boxed().toArray(Integer[] :: new);
+        Arrays.sort(s, Collections.reverseOrder());
+
+        for(int i=0; i<s.length; i++){
+            if((i + 1) % m == 0){
+                answer += score[i] * m;
+            }
+        }
+
+        return answer;
+    }
+}
